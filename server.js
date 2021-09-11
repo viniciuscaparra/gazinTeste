8c6970ac04643866cd125a01f9abebc83d68c3df
viniciuscaparra <viniciuscaparra@gmail.com>
2021-09-11 00:04:22 -0300

    express static

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+    res.render(express.static('index.ejs'));
 })
 
 app.post('/show', (req, res) => {
